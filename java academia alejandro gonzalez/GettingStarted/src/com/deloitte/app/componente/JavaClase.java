d1cfb8ff13a21e3493c3a807e6375810ae820086
goncastorena <agonzalezcastorena@externosdeloittemx.com>
2019-09-10 17:47:35 -0500

    added first project

--- java academia alejandro gonzalez/GettingStarted/src/com/deloitte/app/componente/JavaClase.java
+++ java academia alejandro gonzalez/GettingStarted/src/com/deloitte/app/componente/JavaClase.java
@@ -5,7 +5,45 @@ public class JavaClase {
 	public static void main(String[] args) {
 		
 		System.out.println("Java Class");
-	
+		System.out.println(suma(5,8));
+		System.out.println(resta(5,8));
+		System.out.println(multiplicacion(5,8));
+		System.out.println(division(5,8));
+		System.out.println(modulo(17,8));
+		System.out.println(potencia(2,3));
+		System.out.println(porcentaje(5,8));
+		
 	}	
 	
+	public static int suma (int a,int b)
+	{
+		return  a+b;	
+	}
+	
+	public static int resta (int a,int b)
+	{
+		return a-b;	
+	}
+	
+	public static int multiplicacion (int a,int b)
+	{
+		return a*b;	
+	}
+	public static int division (int a,int b)
+	{
+		return a/b;	
+	}
+	
+	public static double modulo (double a,double b)
+	{
+		return a%b;	
+	}
+	public static double potencia (double a,double b)
+	{
+		return Math.pow(a, b);	
+	}
+	public static double porcentaje (double a,double b)
+	{
+		return (a*b)/100;	
+	}
 }
