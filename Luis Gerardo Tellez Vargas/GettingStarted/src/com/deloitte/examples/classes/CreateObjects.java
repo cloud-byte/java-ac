9ca916ece82294201061e0b5f087e7577f757005
luistellez <luitellez@externosdeloittemx.com>
2019-09-11 13:03:35 -0500

    Ejercicio Suma

---
+++ Luis Gerardo Tellez Vargas/GettingStarted/src/com/deloitte/examples/classes/CreateObjects.java
@@ -0,0 +1,47 @@
+package com.deloitte.examples.classes;
+
+public class CreateObjects {
+
+	public static void main (String [] args) {
+		Person person=new Person ("Luis Gerardo", "Tellez", "Vargas");
+		
+		String nombre=person.getNombre();
+		System.out.println(nombre);
+		
+		nombre="Albano";
+		System.out.println(nombre);
+		String nombre2= new String ("Albano");
+		/*
+		if(charValue == 'a') {
+			System.out.println("Soy Albano");
+		}
+		else {
+			System.out.println("No soy Albano");
+		}
+		
+		for(char i='A'; i <= 'z'; i++)
+		{
+			if (i<= 'Z' || i >= 'a') {
+				System.out.println(i);
+			}
+		}
+		*/
+		
+		char i=65;
+		/*
+		do{
+			if(i <= 'Z' || i>= 'a') {				
+				System.out.println(i);
+			}
+			i++;
+		}while(i<='z');
+		*/
+		
+		while(i<='z') {
+			if (i<='Z' || i>='a') {
+				System.out.println(i);
+			}
+			i++;
+		};
+	}
+}
