396581f2be53a179ddbc20b6ca48c9ee809449f2
Misael Anubis Morales Martinez <mismorales@externosdeloittemx.com>
2019-09-11 12:55:47 -0500

    Test Cases with Primitive Data Types

---
+++ Java Academia Misael Morales/GettingStarted/src/com/deloitte/variableAndMethods/PrimitiveDataTypes.java
@@ -0,0 +1,14 @@
+package com.deloitte.variableAndMethods;
+
+public class PrimitiveDataTypes {
+	
+	boolean boolValue = true; //true or false
+	byte byteValue = 127; //-128 to 127
+	char charValue = 97; //0 to 65535
+	short shortValue = 32767; //-32768 to 32767
+	int intValue = 2147483647; //-2147483648 to 2147483647
+	long longValue = 9223372036854775807L; //-9223372036854775808 to 9223372036854775807
+	float floatValue = 3.4028235e38f; //1.17549435e-38 to 3.4028235e38f
+	double doubleValue = 1.7976931348623157e308; //4.9e-324 to 1.7976931348623157e308 
+	
+}
