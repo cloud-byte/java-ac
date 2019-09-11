b289b708678333c583148c488f87be3224306623
luistellez <luitellez@externosdeloittemx.com>
2019-09-11 15:27:44 -0500

    Test Actualizado: tipo datos

--- Luis Gerardo Tellez Vargas/GettingStarted/src/com/deloitte/examples/tests/TestPerson.java
+++ Luis Gerardo Tellez Vargas/GettingStarted/src/com/deloitte/examples/tests/TestPerson.java
@@ -21,26 +21,56 @@ class TestPerson {
 		
 		short a = 127;
 		short b = 3;
-		short res = 130;
+		short resShort = 130;
 		
 		int c= 2;
 		int d= 1;
 		int resInt=3;
-		/*
-		 * Algunos ya tienen valores arriba para no castear otros los voy a modificar
-		 * pero es para tener los dos ejemplos
-		 * Para adentrarnos a la TestData Class...
-		 * */
-		 */
+		
+		char e='c';
+		char f='a';
+		char resChar=196;
+		
+		long g=234524312;
+		long h=452414433;
+		long resLong=686938745;
+		
+		double i=2.34;
+		double j=3.43;
+		double resDouble=5.77;
+		
+		float k=(float) 2.65;
+		float l=(float )-0.31;
+		float resFloat=(float) 2.34;
+		
+		byte m=100;
+		byte n=22;
+		byte resByte= 122;
+		
+		
+		
+		/*CASOS EN LOS QUE LOS TIPOS DE CARACTER SON IGUALES, DECLARADOS ANTERIORMENTE PARA PODER HACER PRUEBAS CON NUMEROS
+		DIFERENTES SIN CAMBIAR LAS PRUEBAS DE assertEquals SIGUIENTES.
+		*/
+		assertEquals(resShort,p.Suma(a,b)); 
 		assertEquals(resInt,p.Suma(c,d));
-		assertEquals('c',p.Suma('a', (char) 2));
-		assertEquals(234524312,p.Suma(234524311,1 ));
-		assertEquals(5.77,p.Suma(2.34, 3.43),0);
-		assertEquals(res,p.Suma(a,b));
-		assertEquals(2.34,p.Suma((float) 2.65,(float) -0.31),0.05);
-		assertEquals(122,p.Suma((byte) 100, (byte)22));
+		assertEquals(resChar,p.Suma(e,f));
+		assertEquals(resLong,p.Suma(g,h ));
+		assertEquals(resDouble,p.Suma(i, j),0);
+		assertEquals(resFloat,p.Suma(k,l),0.05);
+		assertEquals(resByte,p.Suma(m, n));
 		
 		
+		//CASOS EN LOS QUE LOS TIPOS DE CARACTER SON DIFERENTES, NO VA A PASAR EL TEST.
+		/*
+		assertEquals(resShort,p.Suma(a,d)); 
+		assertEquals(resInt,p.Suma(a,c));
+		assertEquals(resChar,p.Suma(e,g));
+		assertEquals(resLong,p.Suma(g,f ));
+		assertEquals(resDouble,p.Suma(i, l),0);
+		assertEquals(resFloat,p.Suma(k,j),0.05);
+		assertEquals(resByte,p.Suma(m, a));
+		*/
 		
 	}
 	
