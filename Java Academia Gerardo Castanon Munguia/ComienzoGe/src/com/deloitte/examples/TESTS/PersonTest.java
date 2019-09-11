0d78f1ba8d9c2b9c912050dd12f3324e0cf7b32c
Gerardo Castañon <gerardocasta9@gmail.com>
2019-09-11 15:13:23 -0500

    Actualización

--- Java Academia Gerardo Castanon Munguia/ComienzoGe/src/com/deloitte/examples/TESTS/PersonTest.java
+++ Java Academia Gerardo Castanon Munguia/ComienzoGe/src/com/deloitte/examples/TESTS/PersonTest.java
@@ -16,37 +16,37 @@ class PersonTest {
     @Test
 	public void Testsuma() {
 	Person p = new Person(2.3,4.5);
-	p.Suma(2.3,4.5);
+	assertEquals(2.3,4.5, p.Suma(2.3,4.5));
 	}
     @Test
 	public void Testsuma2() {
 	Person p = new Person(2,4);
-	p.Suma(2,4);
+	assertEquals(6, p.Suma(2,4));
 	}
     @Test
 	public void Testsuma3() {
 	Person p = new Person('a','b');
-	p.Suma('a','b');
+	assertEquals('a','b', p.Suma('a','b'));
 	}
     @Test
 	public void Testsuma4() {
 	Person p = new Person('a','b');
-	p.Suma4('a','b');
+	assertEquals('a','b', p.Suma('a','b'));
 	}
     @Test
 	public void Testsuma5() {
 	Person p = new Person(32732,32732);
-	p.Suma5(32732,32732);
+	assertEquals(65464, p.Suma(32732,32732));
 	}
     @Test
 	public void Testsuma6() {
-	Person p = new Person('a','b');
-	p.Suma6(13338,345435456);
+	Person p = new Person(13338,345435456);
+	assertEquals(345448794, p.Suma(13338,345435456));
 	}
     @Test
 	public void Testsuma7() {
-	Person p = new Person(127,127);
-	p.Suma7(127,128);
+	Person p = new Person(127,128);
+	assertEquals(255, p.Suma(127,128));
 	}
     
     
