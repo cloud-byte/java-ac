ca276094424512585d6712a27b122aef05bf7770
goncastorena <agonzalezcastorena@externosdeloittemx.com>
2019-09-12 13:59:44 -0500

    added first project

---
+++ java academia alejandro gonzalez/GettingStarted/src/com/deloitte/example/objects/Estudiantes.java
@@ -0,0 +1,9 @@
+package com.deloitte.example.objects;
+
+public class Estudiantes {
+
+	public static void main(String[]args) {
+		Estudiante estudiante= new Estudiante("alejandro","gonzalez","castorena",151041,8.9);
+	}
+
+}
