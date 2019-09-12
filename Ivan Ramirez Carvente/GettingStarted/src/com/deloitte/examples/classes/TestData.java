743e76a35c6c2065ce849909c6520a81b06cdaa1
ivramirez <ivramirez@externosdeloittemx.com>
2019-09-12 10:56:52 -0500

    update

---
+++ Ivan Ramirez Carvente/GettingStarted/src/com/deloitte/examples/classes/TestData.java
@@ -0,0 +1,33 @@
+package com.deloitte.examples.classes;
+
+public class TestData {
+	
+		public static int aInt [] = {-2,2,3,4,5};
+		public static int bInt [] = {1,5,6,9,6};
+		public static int rInt [] = {-1,7,9,15,11};
+			
+		public static byte aByte [] = {1,2,3,4,5};
+		public static byte bByte [] = {2,3,4,5,6};
+		public static byte rByte [] = {3,5,7,9,11};
+			
+		public static char aChar [] = {4,5,6};
+		public static char bChar [] = {9,3,5};
+		public static char rChar [] = {15,8,11};
+			
+		public static short aShort [] = {1,2,3};
+		public static short bShort [] = {3,4,5};
+		public static short rShort [] = {4,6,8};
+			
+		public static long aLong [] = {23,43,65};
+		public static long bLong [] = {12,76,23};
+		public static long rLong [] = {35,119,88};
+			
+		public static float aFloat [] = {1.2f,3.3f,1.1f};
+		public static float bFloat [] = {2.3f,6.5f,3.1f};
+		public static float rFloat [] = {3.5f,9.8f,4.2f};
+			
+		public static boolean aBool [] = {true, true, false, false};
+		public static boolean bBool [] = {true, false, false, true};
+		public static boolean rBool [] = {true, false, false, false};
+
+}
